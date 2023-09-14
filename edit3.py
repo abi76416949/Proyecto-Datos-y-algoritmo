b5023c872db1c16878f4fc13f9fd6133f78efa18
Abigail Durand Palacios <127162244+abi76416949@users.noreply.github.com>
2023-09-14 11:17:05 -0500

    dad
    adfa

--- edit3.py
+++ edit3.py
@@ -10,14 +10,32 @@ import time
 #driver = webdriver.Chrome()
 
 options = Options()
-numero_telefono = "51997322971"
+numero_telefono = "917628284"
 options = webdriver.ChromeOptions()
 #options.add_argument("--user-data-dir=./driver/data3")
-options.add_argument("--user-data-dir=C:\\Users\\USER\\AppData\\Local\\Google\\Chrome\\User Data")
+options.add_argument("--user-data-dir=C:\\Users\\USER\\AppData\\Local\\Google\\Chrome SxS\\User Data")
 options.add_argument("--profile-directory=Default")
 driver = webdriver.Chrome(options=options) 
 
+def enviarImagen(ruta_imagen):
+    print("se entro a enviar imagen")
+    # Localiza el icono de clip para adjuntar un archivo
+    clip_icon = driver.find_element(By.CSS_SELECTOR,"span[data-icon='attach-menu-plus']")
+    clip_icon.click()
+    print("se selecciono el icono correctamente")
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "span[data-icon='attach-menu-plus']")))
 
+    clip_icon2= driver.find_element(By.CSS_SELECTOR, "div[class='tvf2evcx m0h2a7mj lb5m6g5c j7l1k36l ktfrpxia nu7pwgvd p357zi0d dnb887gk gjuq5ydh i2cterl7 fhf7t426 sap93d0t gndfcl4n ajgl1lbb lniyxyh2 fooq7fky bcfko8ch']").send_keys(ruta_imagen)
+    clip_icon2.click()
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div[class='tvf2evcx m0h2a7mj lb5m6g5c j7l1k36l ktfrpxia nu7pwgvd p357zi0d dnb887gk gjuq5ydh i2cterl7 fhf7t426 sap93d0t gndfcl4n ajgl1lbb lniyxyh2 fooq7fky bcfko8ch']")))
+
+    print("se selecciono el icono2 correctamente")
+
+  
+
+    time.sleep(1)
+
+    clip_icon2.send_keys(Keys.ENTER)
 
 
 time.sleep(1)
@@ -43,3 +61,5 @@ message_box.send_keys(Keys.ENTER)
 time.sleep(5)
 
 print(f"Mensaje enviado a {numero_telefono}: {mensaje}")
+
+enviarImagen("G:\portadas\IMAGEN PORTADA.png")

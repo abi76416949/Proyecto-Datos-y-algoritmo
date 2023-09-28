7a77d76a87c6eada6be0595ef9a41193ba2d0d62
Abigail Durand Palacios <127162244+abi76416949@users.noreply.github.com>
2023-09-28 16:55:05 -0500

    cambiso

--- PROYECTO/main.py
+++ PROYECTO/main.py
@@ -1,48 +1,7 @@
 from registro_persona import RegistroPersona
 from persona import Persona
-<<<<<<< HEAD
-import sys
-
-def Agregar_persona(registro):
-    nombre = input("Nombre y apellido: ")
-    edad = int(input("Edad: "))
-    codigo = input("Código: ")
-    correo = input("Correo electrónico (sin @gmail.com): ")
-    correo = correo + "@gmail.com"
-    numero = input("Número de teléfono: ")
-    genero = input("Género (F/M): ")
-    fecha_nacimiento = input("Fecha de nacimiento (dd/mm/aaaa): ")
-
-    persona = Persona(nombre, codigo, edad, correo, numero, genero, fecha_nacimiento)
-    registro.agregar_persona(persona)
-
-def Eliminar_persona(registro):
-    codigo = input("Ingrese el codigo de la persona a eliminar: ")
-    registro.eliminar_persona(codigo)
-
-def Mostrar_personas(registro):
-    registro.mostrar_personas()
-
-def Buscar_persona(registro):
-    codigo = input("Ingrese el codigo de la persona a buscar: ")
-    registro.buscar_persona(codigo)
-
-def exit_program():
-    print("Saliendo del programa...")
-    sys.exit(0)
-
-# Diccionario
-opciones = {
-    "1": Agregar_persona,
-    "2": Eliminar_persona,
-    "3": Mostrar_personas,
-    "4": Buscar_persona,
-    "0": exit_program
-}
-=======
 from base_de_datos import BaseDeDatos
 from enviar import EnviadorDeCorreos
->>>>>>> tevis
 
 def main():
     registro = RegistroPersona()
@@ -56,9 +15,6 @@ def main():
         print("4. Buscar persona")
         print("0. Salir")
 
-<<<<<<< HEAD
-        seleccion = input("Seleccione una opción: ")
-=======
         opcion = input("Seleccione una opción: ")
         
         if opcion == "1":
@@ -102,10 +58,8 @@ def main():
         elif opcion == "5":
             print("Saliendo del programa...")
             break
->>>>>>> tevis
 
-        if seleccion in opciones:
-            opciones[seleccion](registro)
+        
         else:
             print("Opción no válida. Por favor, seleccione una opción válida.")
 
